d5a0c8e5ee4f047da59500608dca7673a5554622
lauracbf <laurarroncbf@gmail.com>
2019-11-08 09:42:14 -0600

    Segundo commit

--- GDL004-cipher/src/cipher.js
+++ GDL004-cipher/src/cipher.js
@@ -1,3 +1,32 @@
 window.cipher = {
   // Acá escribe tu código.
+  encode: (offset, string) => {
+  var prueba1 = '';
+  for(let i = 0; i < string.length; i ++){
+    var toAscii = string[i].charCodeAt();
+    if(toAscii >= 65 && toAscii <= 90){
+      prueba1 += String.fromCharCode((toAscii - 65 + offset) % 26 +65);
+    }else if (toAscii >= 97 && toAscii <= 122){
+      prueba1 += String.fromCharCode((toAscii - 97 + offset) % 26 +97);
+    }else{
+      prueba1 += string[i];
+    }
+  }
+return prueba1;
+},
+
+decode: (offset, string) => {
+  var prueba2 = '';
+for(let i=0; i<string.length; i++){
+var toAscii2 = string[i].charCodeAt();
+if(toAscii2 >= 65 && toAscii2 <= 90){
+  prueba2 += String.fromCharCode((toAscii2 + 65 - offset) % 26 +65);
+}else if (toAscii2 >= 97 && toAscii2 <= 122){
+  prueba2 += String.fromCharCode((toAscii2 -122 - offset) % 26 +122);
+}else{
+  prueba2 += string[i];
+}
+}
+return prueba2;
+},
 };
--- GDL004-cipher/src/index.js
+++ GDL004-cipher/src/index.js
@@ -1,13 +1,12 @@
 var clic = 1;
-
 //Función para el botón Instrucciones
-function showInstructions(){
 
+document.getElementById('click1').addEventListener('click', showInstructions);
+function showInstructions(){
   if(clic==1){
     document.getElementById('instructions').style.display = "block";
     clic = clic + 1;
   }
-
  else{
    document.getElementById('instructions').style.display = "none";
    clic = 1;
@@ -15,24 +14,44 @@ function showInstructions(){
 }
 
 //Función para el botón Saber más...
+document.getElementById('button1').addEventListener('click', showMore);
 function showMore(){
-
   if(clic==1){
     document.getElementById('sectionMore').style.display = "block";
     clic = clic + 1;
   }
-
  else{
    document.getElementById('sectionMore').style.display = "none";
    clic = 1;
  }
 }
 
-//Función de la barra deslisable
+//Función de la barra deslisable.
 var slider = document.getElementById('slider');
 var val = document.getElementById('value');
 val.innerHTML = slider.value;
 
-slider.oninput = function(){
-  val.innerHTML=this.value;
+document.getElementById('slider').addEventListener('input', valueChange);
+function valueChange(){
+  var valueOne = slider.value;
+document.getElementById('value').innerHTML = valueOne;
+return valueOne;
+}
+
+//Obteniendo valor de textarea al dar clic en botón cifrar
+
+document.getElementById('cifrar').addEventListener('click', obtainText);
+function obtainText(){
+var textOne = document.getElementById('firstText').value ;
+var offset = parseInt(valueChange ());
+document.getElementById('secondText').innerHTML = window.cipher.encode(offset, textOne);
+}
+
+//Obteniendo valor de textarea al dar clic en botón descifrar
+
+document.getElementById('descifrar').addEventListener('click', obtainText2);
+function obtainText2(){
+var textOne = document.getElementById('firstText').value ;
+var offset = parseInt(valueChange ());
+document.getElementById('secondText').innerHTML = window.cipher.decode(offset, textOne);
 }
